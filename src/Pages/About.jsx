61a50685776aad6bda3c97b767428b044afed204
Julia Faraj <julia.abbdi@hotmail.com>
2025-11-01 15:51:42 +0100

    About: sidan klar – återanvänder komponenter från Home

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import AboutUsHero from '../Components/AboutUsHero'
 import HeroSub from '../Components/HeroSub'
+import Brands from '../Components/Brands.jsx';
+import Testimonials from '../Components/Testimonials.jsx';
+import WhychooseUs from '../Components/WhychooseUs.jsx';
+import Subscribe from '../Components/Subscribe.jsx';
+
+
+
+
 
 
 
@@ -10,6 +18,16 @@ const About = () => {
     <>
     <AboutUsHero />
     <HeroSub />
+        <Brands />
+            <Testimonials />
+                <WhychooseUs />
+                    <Subscribe />
+
+                
+
+
+
+
     </>
   )
 }
